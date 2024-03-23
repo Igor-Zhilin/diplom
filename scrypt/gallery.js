8643bf51765eb3b92ea8fe8a506085a44bcd75d3
Igor-Zhilin <izilin33@gmail.com>
2024-03-23 17:24:20 +0300

    commit

--- scrypt/gallery.js
+++ scrypt/gallery.js
@@ -62,24 +62,36 @@ function openModal(city) {
   // Добавляем информацию о городе
   switch(city) {
     case 'Белгород':
-      cityInfo.textContent = "Белгород — административный центр Белгородской области.";
+      cityInfo.innerHTML = `
+      <p><strong>Адрес:</strong><br>Улица 50-летия Белгородской области, 12а</p>
+      <p><strong>Телефон:</strong><br>+7 (904) 088-88-88</p>
+      <p><strong>График работы:</strong><br>ВТ-ПТ: с 10 до 18 ч.<br>СБ: с 10 до 14 ч.<br>ВС-ПН: выходной</p>`;
       cityInfo.style.maxWidth = "700px"; // Установка максимальной ширины текста
       cityMap.innerHTML = '<embed src="https://yandex.ru/map-widget/v1/?um=constructor%3A9452beb4756ad676ebfc3efdea140aae5f08500aac1c0ecce585c0334eb18286&amp;source=constructor" style="width:100%;height:300px"></embed>';
       break;
     case 'Санкт-Петербург':
-      cityInfo.textContent = "Санкт-Петербург — город федерального значения, культурная столица России.";
+      cityInfo.innerHTML = `
+      <p><strong>Адрес:</strong><br>4-я линия Васильевского острова, 45</p>
+      <p><strong>Телефон:</strong><br>+7 (904) 044-44-44</p>
+      <p><strong>График работы:</strong><br>ВТ-ПТ: с 10 до 18 ч.<br>СБ: с 11 до 18 ч.<br>ВС-ПН: выходной</p>`;
       cityInfo.style.maxWidth = "700px"; // Установка максимальной ширины текста
-      cityMap.innerHTML = '<embed src="https://yandex.ru/map-widget/v1/?um=constructor%3A9452beb4756ad676ebfc3efdea140aae5f08500aac1c0ecce585c0334eb18286&amp;source=constructor" style="width:100%;height:300px"></embed>';
+      cityMap.innerHTML = '<embed src="https://yandex.ru/map-widget/v1/?um=constructor%3A4b4088f179339201531fe55390abb5da6d8aba7918121a963666d59af28c45c0&amp;source=constructor" style="width:100%;height:300px"></embed>';
       break;
     case 'Воронеж':
-      cityInfo.textContent = "Воронеж — город на юге Центральной России, административный центр Воронежской области.";
+      cityInfo.innerHTML = `
+      <p><strong>Адрес:</strong><br>Московский проспект, 6</p>
+      <p><strong>Телефон:</strong><br>+7 (996) 436-36-36</p>
+      <p><strong>График работы:</strong><br>Ежедневно с 10 до 19 ч.</p>`;
       cityInfo.style.maxWidth = "700px"; // Установка максимальной ширины текста
-      cityMap.innerHTML = '<embed src="https://yandex.ru/map-widget/v1/?um=constructor%3A9452beb4756ad676ebfc3efdea140aae5f08500aac1c0ecce585c0334eb18286&amp;source=constructor" style="width:100%;height:300px"></embed>';
+      cityMap.innerHTML = '<embed src="https://yandex.ru/map-widget/v1/?um=constructor%3A4b4088f179339201531fe55390abb5da6d8aba7918121a963666d59af28c45c0&amp;source=constructor" style="width:100%;height:300px"></embed>';
       break;
     case 'Калининград':
-      cityInfo.textContent = "Калининград — город в России, административный центр Калининградской области, являющийся самым западным областным центром Российской Федерации.";
+      cityInfo.innerHTML = `
+      <p><strong>Адрес:</strong><br>Улица 9 Апреля, 5</p>
+      <p><strong>Телефон:</strong><br>+7 (966) 039-39-39</p>
+      <p><strong>График работы:</strong><br>Ежедневно с 10 до 19 ч.</p>`;
       cityInfo.style.maxWidth = "700px"; // Установка максимальной ширины текста
-      cityMap.innerHTML = '<embed src="https://yandex.ru/map-widget/v1/?um=constructor%3A9452beb4756ad676ebfc3efdea140aae5f08500aac1c0ecce585c0334eb18286&amp;source=constructor" style="width:100%;height:300px"></embed>';
+      cityMap.innerHTML = '<embed src="https://yandex.ru/map-widget/v1/?um=constructor%3A4b4088f179339201531fe55390abb5da6d8aba7918121a963666d59af28c45c0&amp;source=constructor" style="width:100%;height:300px"></embed>';
       break;
     // Для других городов аналогично добавьте нужные информацию и embed
     default:
